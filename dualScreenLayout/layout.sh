7b586fd8cfc364f6c55a0bab94efc5c4df04e311
mfm6 <melissafm24@gmail.com>
2016-06-07 12:18:40 -0400

    dual monitor screen layout

---
+++ dualScreenLayout/layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate right --output DVI-I-1 --off --output DVI-I-0 --mode 1920x1080 --pos 1080x464 --rotate normal --output DP-1 --off --output DP-0 --off
