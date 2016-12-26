a163d493f4f1f9eaa660742a9f4adce42f95d109
Melissa Mozifian <melissafm24@gmail.com>
2016-12-25 19:17:39 -0500

    install apps

--- init.sh
+++ init.sh
@@ -1,5 +1,36 @@
 #/bin/sh
+set -e # exit on first error
 
+install_dependencies()
+{
+    sudo apt-get update
+    sudo apt-get install -y \
+        git \
+        vim \
+        i3 \
+        xinit \
+        xbacklight \
+        network-manager-gnome \
+        thunar \
+        gnome-icon-theme-full \
+        pavucontrol \
+        v4l-utils
+}
+
+install_dropbox()
+{
+    cd $HOME
+    wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+    ~/.dropbox-dist/dropboxd &
+    cd -
+}
+
+git_config()
+{
+    git config --global user.name "Melissa Mozifian"
+    git config --global user.email "melissafm24@gmail.com"
+    git config --global push.default matching
+}
 
 init_dotfiles()
 {
@@ -7,11 +38,13 @@ init_dotfiles()
     echo "remove old dotfiles"
     rm -rf $HOME/.vim
     rm -f $HOME/.vimrc
+    rm -f $HOME/.screenlayout
 
     # SYMLINKS
     echo "symlinks dotfiles"
     ln -fs $PWD/vim $HOME/.vim
     ln -fs $PWD/vim/vimrc $HOME/.vimrc
+    ln -fs $PWD/screenlayout $HOME/.screenlayout
 
     return 0;
 }
@@ -20,9 +53,9 @@ init_vim()
 {
     echo "install vim plugins"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/
-    git submodule init
-    git submodule update
+    git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+    #git submodule init
+    #git submodule update
     vim -c VundleInstall -c quitall
     exec ./vim/bundle/fzf/install --all
 
@@ -31,6 +64,9 @@ init_vim()
 
 init()
 {
+    #install_dependencies
+    #install_dropbox
+    #git_config
     init_dotfiles
     init_vim
     echo "Done! :)"
--- postInstall.sh
+++ postInstall.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-apt-get update
-sudo apt-get install thunar-archive-plugin
-sudo apt-get install Gnome-icon-theme-full
-
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
