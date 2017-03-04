c7ef8bbb2defa4b7cb4ae3682d883880540479e2
Melissa Mozifian <melissafm24@gmail.com>
2017-03-03 23:26:32 -0500

    updated init file

--- init.sh
+++ init.sh
@@ -5,8 +5,10 @@ install_dependencies()
 {
     sudo apt-get update
     sudo apt-get install -y \
+	xterm \
         git \
-        vim \
+	exuberant-ctags \
+        vim-nox \
         i3 \
         xinit \
         xbacklight \
@@ -17,6 +19,23 @@ install_dependencies()
         v4l-utils
 }
 
+install_tmux()
+{
+    cd /tmp
+    sudo apt-get install -y \
+        libevent-2.0-5 \
+        libevent-dev \
+        libncurses5-dev \
+        libncursesw5-dev
+
+    git clone https://github.com/tmux/tmux.git
+    cd tmux
+    sh autogen.sh
+    ./configure && make
+    cp tmux /usr/local/bin
+    cd ..
+}
+
 install_dropbox()
 {
     cd $HOME
@@ -25,6 +44,23 @@ install_dropbox()
     cd -
 }
 
+install_chrome()
+{
+     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+     sudo apt-get update 
+     sudo apt-get install google-chrome-stable
+}
+
+install_tensorflow()
+{
+     sudo apt-get install python-pip
+     sudo apt-get install python-pip python-dev
+     sudo pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
+
+
+
+}
 git_config()
 {
     git config --global user.name "Melissa Mozifian"
@@ -44,7 +80,7 @@ init_dotfiles()
     echo "symlinks dotfiles"
     ln -fs $PWD/vim $HOME/.vim
     ln -fs $PWD/vim/vimrc $HOME/.vimrc
-    ln -fs $PWD/screenlayout $HOME/.screenlayout
+    #ln -fs $PWD/screenlayout $HOME/.screenlayout
 
     return 0;
 }
@@ -54,8 +90,8 @@ init_vim()
     echo "install vim plugins"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-    #git submodule init
-    #git submodule update
+    git submodule init
+    git submodule update
     vim -c VundleInstall -c quitall
     exec ./vim/bundle/fzf/install --all
 
@@ -64,12 +100,16 @@ init_vim()
 
 init()
 {
-    #install_dependencies
-    #install_dropbox
-    #git_config
+    install_dependencies
+    install_tmux
+    install_dropbox
+    install_chrome
+    git_config
+
     init_dotfiles
     init_vim
     echo "Done! :)"
 }
 
+# MAIN
 init
