ea39826a04157ca86a47f08900f4656ebbc6fa3e
Melissa Mozifian <melissafm24@gmail.com>
2023-12-01 22:08:20 -0800

    update configs and init script

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-#/bin/sh
+#!/bin/sh
 set -e # exit on first error
 
 install_dependencies()
 {
-    sudo apt-get update
-    sudo apt-get install -y \
-	xterm \
+    sudo apt update
+    sudo apt install -y \
+        xterm \
         git \
-	    exuberant-ctags \
+        exuberant-ctags \
         vim-nox \
         i3 \
         xinit \
@@ -20,45 +20,20 @@ install_dependencies()
         thunar-archive-plugin \
         xfce4-screenshooter \
         tmux \
-        redshift redshift-gtk \
-	python-matplotlib \
-	arandr \
-	xclip \
-	sshfs \
-	meld \
-	htop \
-	feh
-}
-
-
-install_dropbox()
-{
-    cd $HOME
-    wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-    ~/.dropbox-dist/dropboxd &
-    cd -
-}
-
-install_chrome()
-{
-     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-     sudo apt-get update
-     sudo apt-get install google-chrome-stable
-}
-
-install_tensorflow()
-{
-     sudo apt-get install python-pip python-dev
-     sudo pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.9.0-cp35-cp35m-linux_x86_64.whl
+        redshift-gtk \
+        python3-matplotlib \
+        arandr \
+        xclip \
+        sshfs \
+        meld \
+        htop \
+        feh
 }
 
 install_python_pckgs()
 {
-    sudo apt-get install python-pip python-dev
-    sudo pip install virtualenv
+    sudo apt install python3-pip python3-dev
 }
-
 
 git_config()
 {
@@ -79,7 +54,6 @@ init_dotfiles()
     echo "symlinks dotfiles"
     ln -fs $PWD/vim $HOME/.vim
     ln -fs $PWD/vim/vimrc $HOME/.vimrc
-    #ln -fs $PWD/screenlayout $HOME/.screenlayout
 
     return 0;
 }
@@ -94,19 +68,11 @@ init_vim()
     return 0;
 }
 
-install_python_dev()
-{
-    echo "install python3 dev and pip3"
-    sudo apt-get install python3-pip python3-dev
-}
-
 init()
 {
     install_dependencies
     install_python_pckgs
-    # install_dropbox
     git_config
-    install_python_dev
     init_dotfiles
     init_vim
     echo "Done! :)"
